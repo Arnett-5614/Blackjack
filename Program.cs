4f2097b5f6fb2d6b2bf6012353fc3aee796150f8
Arnett-5614 <42777763+Arnett-5614@users.noreply.github.com>
2018-09-06 12:42:14 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] cards = new int[10];
+            int[] AllowedValues = new int[] { };
+            Random rand = new Random();
+
+            int GetRandomNumber()
+            {
+                return AllowedValues[rand.Next(AllowedValues.Length)];
+            }
+
+            void myButton_Click(object sender, EventArgs e)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.WriteLine(GetRandomNumber());
+                }
+            }
+
+
+        }
+
+    }
+}
